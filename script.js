dc1886565f30b8cd2d495eb3383c01f73f0a2a15
mayaramachado <mayara_b2k@hotmail.com>
2022-03-28 14:27:56 -0300

    Inicio do portifólio pessoal

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // ready - é disparado depois que o dicumento HTML foi carregado de forma mais rapida sem esperar todo conteúdo ser carregado. Ele é especifico do Jquery
+    $(window).scroll(function(){
+    // Chama o efeito de rolagem e abaixo indica o momento que essa rolagem precisa ser feita. Nesse caso quando for maior que 20. 
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+            // Estou adicionando uma class.
+
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});
