d4dd69ab5b10cdc53928d8f059e0f29ab2e86377
mayaramachado <mayara_b2k@hotmail.com>
2022-04-04 07:27:45 -0300

    Atualização

--- script.js
+++ script.js
@@ -16,4 +16,10 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        
+    });
+    // 
 });
