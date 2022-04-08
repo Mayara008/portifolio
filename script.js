349fe0935f64944df029779d85019d3f44ca82e8
mayaramachado <mayara_b2k@hotmail.com>
2022-04-08 01:34:29 -0300

    Alterações

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 $(document).ready(function(){
     // ready - é disparado depois que o dicumento HTML foi carregado de forma mais rapida sem esperar todo conteúdo ser carregado. Ele é especifico do Jquery
     $(window).scroll(function(){
-    // Chama o efeito de rolagem e abaixo indica o momento que essa rolagem precisa ser feita. Nesse caso quando for maior que 20. 
+        // Chama o efeito de rolagem e abaixo indica o momento que essa rolagem precisa ser feita. Nesse caso quando for maior que 20.
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-            // Estou adicionando uma class.
-
+            
         }else{
             $('.navbar').removeClass("sticky");
         }
-        // Seta no decorrer do portifólio
-        if(this.scroll > 500){
-            $('.scrool-up-btn').addClass("show");
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
         }else{
-            $('.scrool-up-btn').removeClass("show");
+            $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    // slide-up script 
-    $('.scrool-up-btn').click("function")(){
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-    }
-});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
@@ -29,26 +36,26 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing anim script
+    // typing text animation script
     var typed = new Typed(".typing", {
-        Strings: ["Youtuber","Developer","Blogger","Designer","Freelancer"],
-        typeSpeed:100,
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        Strings: ["Youtuber","Developer","Blogger","Designer","Freelancer"],
-        typeSpeed:100,
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-
 
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
+        autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
